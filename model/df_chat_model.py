244fe895cee4104d8ea432fc3018514ed8eb3498
Vihan Perera <vihanpereraux@gmail.com>
2024-08-16 11:34:43 +0100

    update controller

--- model/df_chat_model.py
+++ model/df_chat_model.py
@@ -7,27 +7,19 @@ load_dotenv()
 import os
 from tinydb import TinyDB
 
-# db = TinyDB('model/chat_db/df_chat_history.json')  
-
-def get_answers(question): 
-    # global db 
-    # db.insert = ({
-    #     'role': 'user',
-    #     'content': str(question)
-    # })
+def get_answers(question):
+    # chat history update
+    db = TinyDB('model/chat_db/df_chat_history.json')
+    db.insert({'role': 'user', 'content': str(question)})
 
     llm = ChatGroq(
         model_name="llama-3.1-70b-versatile", api_key = os.getenv("GROQ_API_KEY"))
-
     df = pd.read_csv('test/data.csv')
     df = SmartDataframe(df, config={"llm": llm})
-    answer = df.chat(question) 
+    instructions = "Don't generate any charts, just give the answer in a string format"
+    answer = df.chat(question + instructions)
     
-    # db.insert = ({
-    #     'role': 'assistant',
-    #     'content': str(answer)
-    # })
+    # chat history update 
+    db.insert({'role': 'assistant', 'content': str(answer)})
 
     return str(answer)
-
-# get_answers()
